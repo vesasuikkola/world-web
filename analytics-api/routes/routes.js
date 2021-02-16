4edffaaa51cfe4c5c56fbb7467df22eff243e452
Vesa Suikkola <vesa.suikkola@gmail.com>
2021-02-16 13:01:08 +0200

    Added some req logging

--- analytics-api/routes/routes.js
+++ analytics-api/routes/routes.js
@@ -3,6 +3,16 @@ import * as controllers from '../controllers/controllers.js';
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  console.log(
+    new Date(Date.now()).toISOString(),
+    ': call to',
+    req.method,
+    req.path
+  );
+  next();
+});
+
 router.get('/', controllers.usage);
 router.get('/views', controllers.getAll);
 router.get('/views/:code', controllers.getOne);
