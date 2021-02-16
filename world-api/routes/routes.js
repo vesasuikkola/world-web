528e95179790fa5e8244c5ef96d68d1fe38f5040
Vesa Suikkola <vesa.suikkola@gmail.com>
2021-02-16 13:01:25 +0200

    Added some req logging

--- world-api/routes/routes.js
+++ world-api/routes/routes.js
@@ -3,6 +3,16 @@ import * as controllers from '../controllers/controllers.js';
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  console.log(
+    new Date(Date.now()).toISOString(),
+    ': call to',
+    req.method,
+    req.path
+  );
+  next();
+});
+
 router.get('/:collection/:code', controllers.getOne);
 router.get('/:collection', controllers.getAll);
 router.get('/', controllers.usage); //FIXME: show this for non-existing collections
