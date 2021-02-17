dc4e587ac8d15335d2882ab2f930480306ad6283
Vesa Suikkola <vesa.suikkola@gmail.com>
2021-02-17 14:29:02 +0200

    Fix registration to expect unique emails

--- api-gw/controllers/authController.js
+++ api-gw/controllers/authController.js
@@ -4,23 +4,28 @@ import bcrypt from 'bcryptjs';
 import config from '../config.js';
 
 export const register = (req, res) => {
-  let hashedPassword = bcrypt.hashSync(req.body.password, 8);
-  User.create(
-    {
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    },
-    (err, user) => {
-      if (err) return res.status(500).send('Registration failed');
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) return res.status(500).send('Internal server error');
+    if (user) return res.status(409).send('Email already in use');
 
-      const token = jwt.sign({ id: user._id }, config.secret, {
-        expiresIn: 86400
-      });
+    let hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    User.create(
+      {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      },
+      (err, user) => {
+        if (err) return res.status(500).send('Registration failed');
 
-      res.status(200).send({ auth: true, token: token });
-    }
-  );
+        const token = jwt.sign({ id: user._id }, config.secret, {
+          expiresIn: 86400
+        });
+
+        res.status(200).send({ auth: true, token: token });
+      }
+    );
+  });
 };
 
 export const login = (req, res) => {
