c85cddf73ca645ba0857dd16bc9dc48f6e78d4ef
Vesa Suikkola <vesa.suikkola@gmail.com>
2021-02-17 12:50:19 +0200

    World-api: added auth

--- world-api/config.js
+++ world-api/config.js
@@ -1,5 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const API_PATHS = {
   analytics_db: {
     views: 'analytics/views'
   }
+};
+
+export const AUTH_SECRET = {
+  secret: process.env.AUTH_SECRET
 };
--- world-api/routes/routes.js
+++ world-api/routes/routes.js
@@ -1,20 +1,11 @@
 import express from 'express';
 import * as controllers from '../controllers/controllers.js';
+import isAuthorized from '../services/requestAuthenticator.js';
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-  console.log(
-    new Date(Date.now()).toISOString(),
-    ': call to',
-    req.method,
-    req.path
-  );
-  next();
-});
-
-router.get('/:collection/:code', controllers.getOne);
-router.get('/:collection', controllers.getAll);
-router.get('/', controllers.usage); //FIXME: show this for non-existing collections
+router.get('/:collection/:code', isAuthorized, controllers.getOne);
+router.get('/:collection', isAuthorized, controllers.getAll);
+router.get('/', isAuthorized, controllers.usage); //FIXME: show this for non-existing collections
 
 export default router;
